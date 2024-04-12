decf7c9afdc6415bf368c70e614c1493dfcdd85d
wachuka7 <rachaelwachuka7@gmail.com>
2024-04-12 15:38:56 +0300

    feat: get requests

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -17,7 +15,36 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Home, '/')
+
+class Newsletters(Resource):
+
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+
+        return response
+
+api.add_resource(Newsletters, '/newsletters')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
